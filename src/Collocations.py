436847f432762d9da28bc2abdb70f5def51bcf5e
Seraj Noureddine <snoureddine01@outlook.com>
2026-09-25 02:35:45 -0400

    Created function count_bigrams

--- src/Collocations.py
+++ src/Collocations.py
@@ -22,4 +22,32 @@ def count_unigrams(infile):
 
         with open("unigrams.txt", "w") as outfile:
             outfile.write(f" {unigrams}")
+
     return unigrams       
+
+def count_bigrams():
+    with open(infile, "r") as file:
+        bigrams = {}
+
+        lines = file.readlines()
+
+        for line in lines:
+            tokens = line.split()
+            
+            for i in range(len(tokens) - 1):
+                bigram = (tokens[i], tokens[i+1])
+
+                if (bigram[0] and all(char in string.punctuation for char in bigram[0])
+                        or bigram[1] and all(char in string.punctuation for char in bigram[1])):
+                    continue
+
+                if bigram in bigrams:
+                    bigrams[bigram] += 1
+                else:
+                    bigrams[bigram] = 12
+                
+
+        with open("bigrams.txt", "w") as outfile:
+            outfile.write(f"{bigrams}")
+            
+    return bigrams
